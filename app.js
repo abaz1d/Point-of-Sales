3a6aec0ba407fc932adb4a74e2951b7c3ca58750
Abaz <abaz.my.id@gmail.com>
2022-09-07 15:05:18 +0700

    add ssl

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 var indexRouter = require('./routes/index')(pool);
